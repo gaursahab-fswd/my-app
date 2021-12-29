e14a713aee11b1588211ae18cee5f45773ea77c7
itprogaur <professionalcodehub@gmail.com>
2021-12-29 23:00:15 +0530

    React landing page

---
+++ src/component/header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+const Headersection=()=> {  
+
+     return(
+    <div className="headersection text-center">
+       <h1 className="text-white text-5xl mt-60 font-semibold">Landing template for Startups</h1>
+       <p style={{color:"#606483"}} className="text-xl	mt-5">Our landing page template works on all devices, so you only have to set it <br /> up once, and get beautiful results forever.</p>
+       <input className="bg-sky-600 hover:bg-sky-700 px-4 py-2 text-white mt-10 font-medium rounded-md	" type='submit' value='GET EARLY ACCESS' />
+    </div>
+    );
+
+    }
+
+
+
+
+
+
+export default Headersection;
